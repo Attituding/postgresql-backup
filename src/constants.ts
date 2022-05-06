cd75bc222a3fb8fa2317c305a7ed2db470d25692
Attituding <84680760+Attituding@users.noreply.github.com>
2022-05-05 23:32:35 -0700

    feat: automatic postgres backups to Google Drive

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const constants = {
+    fileName: 'backup',
+    parentFolder: '16BYoO8NHyhF1nr9M4imAMPt1CGBHnzf9',
+    scopes: ['https://www.googleapis.com/auth/drive'],
+};
---
+++ src/main.ts
@@ -0,0 +1,72 @@
+import 'dotenv/config';
+import {
+    auth,
+    drive as googleDrive,
+} from '@googleapis/drive';
+import { constants } from './constants';
+import cron from 'node-cron';
+import fs from 'node:fs/promises';
+import fsSync from 'node:fs';
+import process from 'node:process';
+import shell from 'shelljs';
+
+(async () => {
+    const credentials = {
+        private_key: process.env.GSERVICEPRIVATEKEY!,
+        client_email: process.env.GSERVICEEMAIL!,
+    };
+
+    const authorization = await auth.getClient({
+        scopes: constants.scopes,
+        credentials: credentials,
+    });
+
+    const drive = googleDrive({
+        version: 'v3',
+        auth: authorization,
+    });
+
+    const path = `${__dirname}/../${constants.fileName}.tar`;
+
+    const database = process.env.PGDATABASE!;
+    const host = process.env.PGHOST!;
+    const port = process.env.PGPORT!;
+    const user = process.env.PGUSER!;
+
+    cron.schedule('*/1 * * * *', async () => {
+        try {
+            const output = shell.exec(`pg_dump -U ${user} -h ${host} -p ${port} -w -F t ${database} > ${constants.fileName}.tar`);
+
+            if (output.includes('error')) {
+                throw new Error(output);
+            } else {
+                const time = new Date().toLocaleString(
+                    undefined,
+                    { hour12: false },
+                );
+
+                const stats = await fs.stat(path);
+
+                console.log(stats.size);
+
+                await drive.files.create({
+                    requestBody: {
+                        name: `${time}.tar`,
+                        parents: [constants.parentFolder],
+                    },
+                    media: {
+                        mimeType: 'application/x-tar',
+                        body: fsSync.createReadStream(path),
+                    },
+                });
+
+                await fs.unlink(path);
+            }
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }, {
+        timezone: 'America/Vancouver',
+    });
+})();
