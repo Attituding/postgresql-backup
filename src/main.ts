f4cb5128fc69a19305d586d4ced108affc0dd503
Attituding <84680760+Attituding@users.noreply.github.com>
2022-05-06 10:40:02 -0700

    test: express

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,15 @@ import {
 } from '@googleapis/drive';
 import { constants } from './constants';
 import cron from 'node-cron';
+import express from 'express';
 import fs from 'node:fs/promises';
 import fsSync from 'node:fs';
 import process from 'node:process';
 import shell from 'shelljs';
 
 (async () => {
+    const app = express();
+
     const credentials = {
         private_key: process.env.GSERVICEPRIVATEKEY!,
         client_email: process.env.GSERVICEEMAIL!,
@@ -26,47 +29,88 @@ import shell from 'shelljs';
         auth: authorization,
     });
 
-    const path = `${__dirname}/../${constants.fileName}.tar`;
+    const basePath = `${__dirname}/../`;
+    const tar = `${basePath}${constants.fileName}.tar`;
 
     const database = process.env.PGDATABASE!;
     const host = process.env.PGHOST!;
+    const password = process.env.PGPASSWORD!;
     const port = process.env.PGPORT!;
     const user = process.env.PGUSER!;
 
+    app.all('/backup', async (req, res) => {
+        if (req.headers.authorization === password) {
+            await attempt(backup);
+
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(401);
+        }
+    });
+
+    app.all('/restore', async (req, res) => {
+        const files = await drive.files.list({
+            includeItemsFromAllDrives: true,
+            pageSize: 1,
+            q: `'${constants.parentFolder}' in parents and trashed = false`,
+        });
+
+        const file = await drive.files.get({
+            fileId: files.data.files![0]!.id!,
+            alt: 'media',
+        });
+
+        console.log(file);
+
+        return;
+
+        await fs.writeFile(`${__dirname}../temp.tar`, file.data);
+
+        const output = shell.exec(`pg_restore -U ${user} -h ${host} -p ${port} -w -F t -d ${database} ${constants.fileName}.tar`);
+    });
+
     cron.schedule('0 0 * * *', async () => {
+        await attempt(backup);
+    }, {
+        timezone: 'America/Vancouver',
+    });
+
+    async function backup() {
+        const output = shell.exec(`pg_dump -U ${user} -h ${host} -p ${port} -w -F t ${database} > ${constants.fileName}.tar`);
+
+        if (output.includes('error')) {
+            throw new Error(output);
+        } else {
+            const time = new Date().toLocaleString(
+                undefined,
+                { hour12: false },
+            );
+
+            const stats = await fs.stat(tar);
+
+            console.log(`${time}.tar ${stats.size} bytes`);
+
+            await drive.files.create({
+                requestBody: {
+                    name: `${time}.tar`,
+                    parents: [constants.parentFolder],
+                },
+                media: {
+                    mimeType: 'application/x-tar',
+                    body: fsSync.createReadStream(tar),
+                },
+            });
+
+            await fs.unlink(tar);
+        }
+    }
+
+    async function attempt(func: () => Promise<void>) {
         try {
-            const output = shell.exec(`pg_dump -U ${user} -h ${host} -p ${port} -w -F t ${database} > ${constants.fileName}.tar`);
-
-            if (output.includes('error')) {
-                throw new Error(output);
-            } else {
-                const time = new Date().toLocaleString(
-                    undefined,
-                    { hour12: false },
-                );
-
-                const stats = await fs.stat(path);
-
-                console.log(`${time}.tar ${stats.size} bytes`);
-
-                await drive.files.create({
-                    requestBody: {
-                        name: `${time}.tar`,
-                        parents: [constants.parentFolder],
-                    },
-                    media: {
-                        mimeType: 'application/x-tar',
-                        body: fsSync.createReadStream(path),
-                    },
-                });
-
-                await fs.unlink(path);
-            }
+            await func();
         } catch (error) {
             console.error(error);
             process.exit(1);
         }
-    }, {
-        timezone: 'America/Vancouver',
-    });
+    }
 })();
