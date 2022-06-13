5e16a56ba7d45c8a6e1131c233631f2d3a9f4075
Attituding <84680760+Attituding@users.noreply.github.com>
2022-06-13 16:30:42 -0700

    feat: query amount check feat: raw query

--- src/server.ts
+++ src/server.ts
@@ -36,6 +36,43 @@ app.all('/backup', async (req, res) => {
     res.sendStatus(200);
 });
 
+app.all('/raw', (req, res) => {
+    const query = req.query.query;
+
+    if (typeof query === 'undefined') {
+        res.status(400).send(`No query provided`);
+
+        return;
+    }
+
+    const queryLength = Object.values(req.query).length;
+
+    if (queryLength !== 2) {
+        res.status(400).send(`Invalid amount of queries: ${queryLength} is not the expected amount of queries.`);
+
+        return;
+    }
+
+    if (typeof query !== 'string') {
+        res.status(400).send(`Invalid query provided: ${query} is not typeof string`);
+
+        return;
+    }
+
+    const output: string = shell.exec(query as string);
+
+    if (output.toLowerCase().includes('error')) {
+        res.status(500).send(`An error occurred while trying to restore.\n\nError:\n${output}`);
+        console.error(new Error(output));
+
+        return;
+    }
+
+    console.log(`Executed query ${query}`);
+
+    res.status(200).send(output);
+});
+
 app.all('/restore/database', async (req, res) => {
     const query = req.query;
 
@@ -45,14 +82,28 @@ app.all('/restore/database', async (req, res) => {
 
     if (constants.databases.includes(String(database)) === false) {
         res.status(400).send(`Invalid database: ${database} is not listed in ${constants.databases.join(', ')}`);
+
+        return;
     }
 
     if (typeof fileID === 'undefined') {
         res.status(400).send(`No fileID provided`);
+
+        return;
+    }
+
+    const queryLength = Object.values(query).length;
+
+    if (queryLength !== 3) {
+        res.status(400).send(`Invalid amount of queries: ${queryLength} is not the expected amount of queries. Did you mean to use /restore/global?`);
+
+        return;
     }
 
     if (typeof fileID !== 'string') {
         res.status(400).send(`Invalid fileID provided: ${fileID} is not typeof string`);
+
+        return;
     }
 
     const drive = await driveExport();
@@ -101,10 +152,22 @@ app.all('/restore/global', async (req, res) => {
 
     if (typeof fileID === 'undefined') {
         res.status(400).send(`No fileID provided`);
+
+        return;
+    }
+
+    const queryLength = Object.values(query);
+
+    if (Number(queryLength) !== 2) {
+        res.status(400).send(`Invalid amount of queries: ${queryLength} is not the expected amount of queries. Did you mean to use /restore/database?`);
+
+        return;
     }
 
     if (typeof fileID !== 'string') {
         res.status(400).send(`Invalid fileID provided: ${fileID} is not typeof string`);
+
+        return;
     }
 
     const drive = await driveExport();
@@ -151,6 +214,7 @@ app.all('*', (_req, res) => {
         `/?auth=password<br>
         <br>
         /backup<br>
+        /raw?auth=password&query=query
         /restore/database?auth=password&database=database&fileID=fileID<br>
         /restore/global?auth=password&fileID=fileID<br>`,
     );
